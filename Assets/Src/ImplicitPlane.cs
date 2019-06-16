4029eed887c06ad759575d21636c405b0d15ce40
MounirTakieddine <MounirTakieddine@bitbucket.org>
2019-06-16 20:09:26 +0100

    Improved boudning volume computation in ImplicitPlane implementation

--- Assets/Src/ImplicitPlane.cs
+++ Assets/Src/ImplicitPlane.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
+using System.Linq;
 
 public class ImplicitPlane : ImplicitSurface
 {
-    public float Width, Height, Depth;
+    public float Width, Depth;
 
     public Vector3 GetUpVector()
     {
@@ -24,9 +25,28 @@ public class ImplicitPlane : ImplicitSurface
 
     public override SurfaceBounds GetSurfaceBounds()
     {
+        Quaternion local_rotation   = transform.localRotation;
+
+        Vector3 position            = new Vector3(0.0f, transform.position.y, 0.0f);
+        Vector3 right               = position + ( local_rotation * Vector3.right ).normalized * Width;
+        Vector3 forward             = position + ( local_rotation * Vector3.forward ).normalized * Depth;
+        Vector3 forward_right       = position + ( local_rotation * new Vector3(1.0f, 0.0f, 1.0f) ).normalized *
+                                                 new Vector3(Width, Depth).magnitude;
+
+        float[] y_magnitudes = new float[4]
+        {
+            position.y,
+            right.y,
+            forward.y,
+            forward_right.y
+        };
+
+        float max_y_magnitude   = y_magnitudes.Max();
+        float min_y_magnitude  = y_magnitudes.Min();
+
         return new SurfaceBounds(
-            new Vector3(Width, Height, Depth),
-            Vector3.zero
+            new Vector3(Width, max_y_magnitude, Depth),
+            new Vector3(0, min_y_magnitude, 0)
         );
     }
 
